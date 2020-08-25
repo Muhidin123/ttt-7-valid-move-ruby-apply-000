9cc2c6d3585d0824e87ef3be51bffc60ff5a9fe2
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-08-25 18:41:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,9 +5,15 @@ def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
       return false
-    else
+    elsif
       return true
+    elsif
+      (position_taken?(board, index)) == false && (on_board?(index) == true)
+      return true
+    else
+      return false
     end
   end
 end
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
